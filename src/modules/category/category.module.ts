0cd42d5bacf985882f42989eb7faf7c254e11fab
Daniel K. Guolo <daniel_kv@hotmail.com>
2020-10-08 16:58:45 -0300

    category module

--- src/modules/category/category.module.ts
+++ src/modules/category/category.module.ts
@@ -1,4 +1,7 @@
 import { Module } from '@nestjs/common';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { CategoryFilterDTO } from './dtos/category.filter.dto';
+import { CategoryRepository } from './repositories/category.repository';
 
-@Module({})
+@Module({ imports: [TypeOrmModule.forFeature([CategoryRepository]), CategoryFilterDTO] })
 export class CategoryModule {}
