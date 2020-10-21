d58dfd8bf942c5785b95b753000c03cd34ffe35e
Daniel K. Guolo <daniel_kv@hotmail.com>
2020-10-21 11:26:48 -0300

    password service

--- src/modules/common/common.module.ts
+++ src/modules/common/common.module.ts
@@ -4,11 +4,12 @@ import { PageInfo } from './types/page-info';
 import { ConfigTransformHelper } from './helpers/config.transform.helper';
 import { AddressHelper } from './helpers/address.helper';
 import { GetRequestHelper } from './helpers/get-request.helper';
+import { PasswordService } from './services/password.service';
 
 @Global()
 @Module({
     imports: [PageInfo],
-    providers: [GeoPointHelper, ConfigTransformHelper, AddressHelper, GetRequestHelper],
-    exports: [ConfigTransformHelper, AddressHelper, GetRequestHelper],
+    providers: [GeoPointHelper, ConfigTransformHelper, AddressHelper, GetRequestHelper, PasswordService],
+    exports: [ConfigTransformHelper, AddressHelper, GetRequestHelper, PasswordService],
 })
 export class CommonModule {}
---
+++ src/modules/common/services/password.service.ts
@@ -0,0 +1,38 @@
+import { Injectable } from '@nestjs/common';
+import * as crypto from 'crypto';
+
+interface ISalted {
+    password: string;
+    salt: string;
+}
+
+@Injectable()
+export class PasswordService {
+    async create(plainTextPassword: string): Promise<string> {
+        const newSalt = crypto.randomBytes(16).toString('hex');
+        const passwordHash = this.salt(plainTextPassword, newSalt);
+        return passwordHash;
+    }
+
+    async compare(plainTextPassword: string, hash: string): Promise<boolean> {
+        const salt = this.break(hash).salt;
+        const comparePasswordHash = this.salt(plainTextPassword, salt);
+        return comparePasswordHash === hash;
+    }
+
+    private salt(plainTextPassword: string, salt: string): string {
+        const hash = crypto.createHmac('sha512', salt);
+        hash.update(plainTextPassword);
+        const _password = hash.digest('hex');
+        return `$${salt}$${_password}`;
+    }
+
+    private break(hash: string): ISalted {
+        const broke = hash.split('$');
+
+        return {
+            salt: broke[0],
+            password: broke[1],
+        };
+    }
+}
