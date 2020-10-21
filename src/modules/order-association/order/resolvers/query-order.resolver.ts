2234a431d67ffd62e8a28f5136d8deb362d9f6d6
Daniel K. Guolo <daniel_kv@hotmail.com>
2020-10-21 10:02:32 -0300

    clear authentication

--- src/modules/order-association/order/resolvers/query-order.resolver.ts
+++ src/modules/order-association/order/resolvers/query-order.resolver.ts
@@ -1,12 +1,4 @@
-import { UnauthorizedException, UseGuards } from '@nestjs/common';
 import { Args, ID, Info, Query, Resolver } from '@nestjs/graphql';
-import { AclRoles } from 'src/modules/auth/decorators/checked-roles.decorator';
-import { AclScopes } from '../../../auth/decorators/acl-scopes.decorator';
-import { UseRoles } from '../../../auth/decorators/use-roles.decorator';
-import { ACLResourcesEnum } from '../../../auth/enums/resources.enum';
-import { ACGuard } from '../../../auth/guards/ac.guard';
-import { IPermissionsScopes, IRole } from '../../../auth/interfaces/guard-roles.interface';
-import { AcCheckService } from '../../../auth/services/validate-roles.service';
 import { ExtractFieldsPipe } from '../../../common/pipes/extract-fields.pipe';
 import { PageInfo } from '../../../common/types/page-info';
 import { OrderFilterDTO } from '../dtos/order.filter.dto';
@@ -22,7 +14,6 @@ export class QueryOrderResolver {
         private countOrdersService: CountOrdersService,
         private listOrdersService: ListOrdersService,
         private getOrderService: GetOrderService,
-        private acCheckService: AcCheckService,
     ) {}
 
     @Query(() => OrdersListDTO)
@@ -42,29 +33,8 @@ export class QueryOrderResolver {
         return list;
     }
 
-    @UseGuards(ACGuard)
-    @UseRoles({
-        action: 'read',
-        resource: ACLResourcesEnum.ORDER,
-        possession: 'own',
-        testOwner({ user }, resource: Order) {
-            return user.userId === resource.userId;
-        },
-    })
     @Query(() => Order)
-    async order(
-        @Args('orderId', { type: () => ID }) orderId: number,
-        @AclScopes() permissionScopes: IPermissionsScopes,
-        @AclRoles() checkedRoles: IRole[],
-    ): Promise<Order> {
-        // get order
-        const order = await this.getOrderService.execute(orderId);
-
-        // validate read:own
-        if (!this.acCheckService.checkOwnerPermission(checkedRoles, permissionScopes, order))
-            throw new UnauthorizedException();
-
-        // return order
-        return order;
+    order(@Args('orderId', { type: () => ID }) orderId: number): Promise<Order> {
+        return this.getOrderService.execute(orderId);
     }
 }
