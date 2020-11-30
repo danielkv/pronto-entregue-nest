0941def27b094b3237c64472ced2db67e9566603
Daniel K. Guolo <daniel_kv@hotmail.com>
2020-11-30 20:43:13 -0300

    option group default sort

--- src/modules/product-association/option-group/option-group.module.ts
+++ src/modules/product-association/option-group/option-group.module.ts
@@ -1,5 +1,6 @@
 import { Module } from '@nestjs/common';
 import { OptionGroupRepository } from './repositories/option-group.repository';
+import { SortDirection } from '@nestjs-query/core';
 import { NestjsQueryGraphQLModule } from '@nestjs-query/query-graphql';
 import { NestjsQueryTypeOrmModule } from '@nestjs-query/query-typeorm';
 import { OptionGroupDTO } from './dtos/option.group.dto';
@@ -8,7 +9,14 @@ import { OptionGroupDTO } from './dtos/option.group.dto';
     imports: [
         NestjsQueryGraphQLModule.forFeature({
             imports: [NestjsQueryTypeOrmModule.forFeature([OptionGroupRepository])],
-            resolvers: [{ DTOClass: OptionGroupDTO, EntityClass: OptionGroupRepository, delete: { disabled: true } }],
+            resolvers: [
+                {
+                    DTOClass: OptionGroupDTO,
+                    EntityClass: OptionGroupRepository,
+                    delete: { disabled: true },
+                    read: { defaultSort: [{ field: 'order', direction: SortDirection.DESC }] },
+                },
+            ],
         }),
     ],
 })
