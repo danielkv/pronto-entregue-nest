7c6dbe801f9ac99a1f0d149f41dd103bfad16bc6
Daniel K. Guolo <daniel_kv@hotmail.com>
2020-11-30 20:44:13 -0300

    product default sort

--- src/modules/product-association/product/product.module.ts
+++ src/modules/product-association/product/product.module.ts
@@ -1,6 +1,7 @@
 import { Module } from '@nestjs/common';
 import { ProductRepository } from './repositories/product.repository';
 import { ProductDTO } from './dtos/product.dto';
+import { SortDirection } from '@nestjs-query/core';
 import { NestjsQueryGraphQLModule } from '@nestjs-query/query-graphql';
 import { NestjsQueryTypeOrmModule } from '@nestjs-query/query-typeorm';
 
@@ -8,7 +9,14 @@ import { NestjsQueryTypeOrmModule } from '@nestjs-query/query-typeorm';
     imports: [
         NestjsQueryGraphQLModule.forFeature({
             imports: [NestjsQueryTypeOrmModule.forFeature([ProductRepository])],
-            resolvers: [{ DTOClass: ProductDTO, EntityClass: ProductRepository, delete: { disabled: true } }],
+            resolvers: [
+                {
+                    DTOClass: ProductDTO,
+                    EntityClass: ProductRepository,
+                    delete: { disabled: true },
+                    read: { defaultSort: [{ field: 'order', direction: SortDirection.DESC }] },
+                },
+            ],
         }),
     ],
 })
